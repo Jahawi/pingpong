12e34d91dc2d81281d52de06cc2a9ca424399936
fadhili <fadhiliodewo@gmail>
2017-08-24 09:53:27 +0300

    Add modiified index.html and js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
-function pingPongClick() {
-  var number = document.getElementById("number").value;
-  var result = [];
-  for (var i = 1; i <= number; i++) {
-    if (i % 15 == 0) {
-      result += "<li>pingpong</li>";
-    }
-    else if (i % 5 == 0) {
-      result += "<li>pong</li>";
-    }
-    else if (i % 3 == 0) {
-      result += "<li>ping</li>";
-    }
-    else {
-      result += "<li>" + i + "</li>";
-   )};
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    $("#userOutput").empty();
+    var userInput = parseInt($("input#number").val());
+    var results = PingPong(userInput);
+    results.forEach(function(output) {
+    $("#result").append("<li>" + output + "</li>");
+    });
+  });
+});
 
-
+//business
+function PingPong(userInput){
+var result = [];
+for (var i = 1; i <= userInput; i++){
+  console.log(i);
+  if ((i % 15) === 0){
+    result.push ("pingpong");
+  } else if ((i % 5) === 0){
+    result.push ("pong");
+  } else if ((i % 3) === 0){
+    result.push ("ping");
+  } else{
+    result.push (i);
+    }
   }
-  $document.getElementById("result").innerHTML = result;
-)};
+  console.log(result);
+  return result;
+}
